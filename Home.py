b74639310620419180857a980ec8ed4ccbfc9195
thomas-ubm <thomas.altheimer@u-bordeaux-montaigne.fr>
2024-12-18 09:22:20 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,3 +6,14 @@ st.title('Interface-Mistral-AI ')
 
 # CRéatin d'un sous titre
 st.subheader ('Mistral')
+
+# Création d'une zone de texte
+
+# Création d'un bouton
+
+
+# Création d'un slider
+
+
+# Lecture d'un fichier csv avec mandas
+
