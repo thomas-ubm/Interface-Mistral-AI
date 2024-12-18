553f5de7a04336913649298930496099982b5998
thomas-ubm <thomas.altheimer@u-bordeaux-montaigne.fr>
2024-12-18 16:14:16 +0100

    Update Chat with Mitral-Agent.py

--- pages/Chat with Mitral-Agent.py
+++ pages/Chat with Mitral-Agent.py
@@ -43,9 +43,7 @@ if user_agent == "Emojibot" :
 
 with open('TEST.txt', mode='w') as f:
     f.write ("test")
-btn = st.download_button(label='Download',
-                                    data=f,
-                                    file_name='TEST.txt')
+st.download_button(label='Download', data=f, file_name='TEST.txt')
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
