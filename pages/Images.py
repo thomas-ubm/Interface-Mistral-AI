b199c8fc479dffe2f1a939c6b7c4a27889749e4f
thomas-ubm <thomas.altheimer@u-bordeaux-montaigne.fr>
2024-12-18 11:30:19 +0100

    Update Images.py

--- pages/Images.py
+++ pages/Images.py
@@ -6,20 +6,6 @@ from mistralai import Mistral
 api_keys = st.sidebar.text_input("apikeys")
 client = Mistral(api_key=api_keys)
 
-st.title("Analyse d'image")
-
-st.subheader("Saisir ici le lien vers l'image à analyser")
-#prompt = st.text_area("")
-img_url = st.text_input()
-# Création d'un bouton
-if st.button("Analyser"):
-  st.write(img_url)
-  response = get_sentiment(client, img_url)
-  st.write(response)
-
-
-img_url = "https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
-
 def get_img(client, img_url):
   response = client.chat.complete(
       #model="mistral-larg-latest",
@@ -43,3 +29,19 @@ def get_img(client, img_url):
       }]
   )
   response.choices[0].message.content
+  
+
+st.title("Analyse d'image")
+
+st.subheader("Saisir ici le lien vers l'image à analyser")
+#prompt = st.text_area("")
+img_url = st.text_input("")
+# Création d'un bouton
+if st.button("Analyser"):
+  st.write(img_url)
+  response = get_sentiment(client, img_url)
+  st.write(response)
+
+
+# img_url = "https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
+
