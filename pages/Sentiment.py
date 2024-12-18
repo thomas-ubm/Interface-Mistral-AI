532e948dbe3c94e735ec5caced13b29d7a39edd8
thomas-ubm <thomas.altheimer@u-bordeaux-montaigne.fr>
2024-12-18 11:13:25 +0100

    Update Sentiment.py

--- pages/Sentiment.py
+++ pages/Sentiment.py
@@ -1,1 +1,18 @@
+import streamlit as st
+from function import *
+from mistralai import Mistral
 
+# Clé à utiliser : trA3qWPCyCxnGFqXozRTOh1bSNMoZmQ2
+api_keys = st.sidebar.text_input("apikeys")
+client = Mistral(api_key=api_keys)
+
+st.title("Traduction")
+
+st.subheader("Saisir ici le texte à traduire")
+prompt = st.text_area("")
+
+# Création d'un bouton
+if st.button("Traduire"):
+  st.write(prompt)
+  response = get_trad(client, prompt)
+  st.write(response)
