0b4f879ecd9a6338ed7c7b735790b0421175eda0
thomas-ubm <thomas.altheimer@u-bordeaux-montaigne.fr>
2024-12-18 10:30:48 +0100

    Update Traduction.py

--- pages/Traduction.py
+++ pages/Traduction.py
@@ -1,3 +1,18 @@
 import streamlit as st
+from function import get_ner
+from mistralai import Mistral
+
+api_keys = st.text_input("apikeys")
+client = Mistral(api_key=api_keys)
+
+prompt = st.text_area("""
+
+
+""")
+
+response = get_ner(client, prompt)
+
 
 st.title('Traduction')
+
+st.write(response)
