c559d94f000c537744d374bd380b9610539257d8
thomas-ubm <thomas.altheimer@u-bordeaux-montaigne.fr>
2024-12-18 09:56:30 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -51,10 +51,8 @@ import pandas as pd
 path_url = "https://raw.githubusercontent.com/Quera-fr/My-Credit/refs/heads/main/Analyse%20des%20donn%C3%A9es/test.csv"
 
 df = pd.read_csv(path_url , sep=';')
-
-st.write(df)
-
-
+# st.write(df)
+edited_df = st.data_editor(df)
 
 
 
